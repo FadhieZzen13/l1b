ed041f7ffceada48b7703268423b61039573efb9
FadhieZzen13 <fadhie1212my@gmail.com>
2025-05-15 15:54:53 +0700

    Delete l1b-system directory

--- l1b-system/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $conn->query("DELETE FROM library WHERE id=$id");
-    echo "Book deleted. <a href='read.php'>Back to list</a>";
-}
-?>
